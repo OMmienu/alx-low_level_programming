45f70f6fb2a38a37390971b6917b6812571dca09
Ommienu <nkedzi@gmail.com>
2022-10-04 15:10:24 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the name of the program
- * @argc: number of argumentspassed to the function
+ * @argc: number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  *
  * Return: always 0
