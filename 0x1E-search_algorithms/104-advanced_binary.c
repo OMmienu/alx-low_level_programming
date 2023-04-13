d7725e9d59b4280311303aa259d671b80de67115
OMmienu <nkedzi@gmail.com>
2023-04-12 17:02:16 -0700

    104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,71 +1,54 @@
 #include "search_algos.h"
 
 /**
-  * _binary_search - Searches for a value in a sorted array
-  *                  of integers using binary search.
+  * advanced_binary_recursive - Searches recursively for a value in a sorted
+  *
   * @array: A pointer to the first element of the array to search.
   * @left: The starting index of the array to search.
   * @right: The ending index of the array to search.
   * @value: The value to search for.
   *
-  * Return: Always return Exit_success
+  * Return: exit success
   *
   *
   * Description: Prints the [sub]array being searched after each change.
   */
-int _binary_search(int *array, size_t left, size_t right, int value)
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
 
-	if (array == NULL)
+	if (right < left)
 		return (-1);
 
-	while (right >= left)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
-	}
-
-	return (-1);
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
 }
 
 /**
-  * exponential_search - Searches for a value in a sorted array
-  *                      of integers using exponential search.
+  * advanced_binary - Searches for a value in a sorted array
+  *
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
   *
-  * Return: Always Return Exit_Success
+  * Return: Exit success
   *
   *
-  * Description: Prints a value every time it is compared in the array.
+  * Description: Prints the array being searched after each change.
   */
-int exponential_search(int *array, size_t size, int value)
+int advanced_binary(int *array, size_t size, int value)
 {
-	size_t i = 0, right;
-
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
-	if (array[0] != value)
-	{
-		for (i = 1; i < size && array[i] <= value; i = i * 2)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-	}
-
-	right = i < size ? i : size - 1;
-	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
-	return (_binary_search(array, i / 2, right, value));
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
-
